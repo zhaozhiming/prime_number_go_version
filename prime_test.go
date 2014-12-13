b22ab91190b467eb9c508db672711d766d67586b
zhaozhiming <kingzzm1982@sina.com>
2014-12-14 06:52:19 +0800

    add second test case: given 2 renturn {2}

--- prime_test.go
+++ prime_test.go
@@ -5,6 +5,10 @@ import (
 	"testing"
 )
 
-func Test_Given_1_then_return_empty_list(t *testing.T) {
+func Test_given_1_then_return_empty_list(t *testing.T) {
 	assert.Equal(t, []int{}, Prime(1))
+}
+
+func Test_given_2_then_return_2(t *testing.T) {
+	assert.Equal(t, []int{2}, Prime(2))
 }
