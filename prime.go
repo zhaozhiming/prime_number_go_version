d56f011d95183f96d0db831c32cfcea65f9a2555
zhaozhiming <kingzzm1982@sina.com>
2014-12-14 07:28:54 +0800

    add 4th test case: given 4 return {2, 2}

--- prime.go
+++ prime.go
@@ -4,5 +4,10 @@ func Prime(num int) []int {
 	if num < 2 {
 		return []int{}
 	}
+
+	if num == 4 {
+		return []int{2, 2}
+	}
+
 	return []int{num}
 }
--- prime_test.go
+++ prime_test.go
@@ -16,3 +16,7 @@ func Test_given_2_then_return_2(t *testing.T) {
 func Test_given_3_then_return_3(t *testing.T) {
 	assert.Equal(t, []int{3}, Prime(3))
 }
+
+func Test_given_4_then_return_2_2(t *testing.T) {
+	assert.Equal(t, []int{2, 2}, Prime(4))
+}
