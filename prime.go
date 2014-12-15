feb7a9143cf31520362a63a1a23019bbebe86cea
zhaozhiming <kingzzm1982@sina.com>
2014-12-15 12:31:00 +0800

    finish

--- prime.go
+++ prime.go
@@ -5,26 +5,15 @@ func Prime(num int) []int {
 		return []int{}
 	}
 
-	if isPrime(num) {
-		return []int{num}
-	}
-
 	result := []int{}
-	div := 2
-	mod := num % div
-	if mod == 0 {
-		result = append(result, div)
-		result = append(result, num/div)
-	}
-
-	return result
-}
-
-func isPrime(num int) bool {
-	for i := 2; i < num; i++ {
-		if num%i == 0 {
-			return false
+	for div := 2; div <= num; {
+		mod := num % div
+		if mod == 0 {
+			result = append(result, div)
+			num /= div
+		} else {
+			div++
 		}
 	}
-	return true
+	return result
 }
--- prime_test.go
+++ prime_test.go
@@ -24,3 +24,27 @@ func Test_given_4_then_return_2_2(t *testing.T) {
 func Test_given_6_then_return_2_3(t *testing.T) {
 	assert.Equal(t, []int{2, 3}, Prime(6))
 }
+
+func Test_given_8_then_return_2_2_2(t *testing.T) {
+	assert.Equal(t, []int{2, 2, 2}, Prime(8))
+}
+
+func Test_given_9_then_return_3_3(t *testing.T) {
+	assert.Equal(t, []int{3, 3}, Prime(9))
+}
+
+func Test_given_20_then_return_2_2_5(t *testing.T) {
+	assert.Equal(t, []int{2, 2, 5}, Prime(20))
+}
+
+func Test_given_30_then_return_2_3_5(t *testing.T) {
+	assert.Equal(t, []int{2, 3, 5}, Prime(30))
+}
+
+func Test_given_64_then_return_2_2_2_2_2_2(t *testing.T) {
+	assert.Equal(t, []int{2, 2, 2, 2, 2, 2}, Prime(64))
+}
+
+func Test_given_10984_then_return_2_2_2_1373(t *testing.T) {
+	assert.Equal(t, []int{2, 2, 2, 1373}, Prime(10984))
+}
